1cdb0c73c581138876d05ebcd1787caab9ef0530
Kun Peng Fan <fkpwolf@gmail.com>
2018-07-23 16:24:35 +0800

    Update main.go

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Printf("title: %s\n", title)
 	}
 
-	fmt.Fprintf(w, "你好啊，朋友😆，👻 from:  "+title+"\n")
+	fmt.Fprintf(w, "你好啊，朋友😆，👻 from:  "+title+" 🤪\n")
 }
 
 func main() {
